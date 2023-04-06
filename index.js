b7fef42ca2af0ac8e5a3b16358f749f421dbb730
FairyPenguin <autorun81@gmail.com>
2023-04-06 15:01:07 +0200

    Adding The Navbar & Update Skills Section

---
+++ index.js
@@ -0,0 +1,23 @@
+// fetch(
+//   "https://newsapi.org/v2/everything?" +
+//     "q=linux&" +
+//     "from=2023-03-31&" +
+//     "sortBy=popularity&" +
+//     "apiKey=7181cc8ec0f04253bf628ce61f1b5ab1"
+// )
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   });
+
+let NormFunc = function () {
+  return function () {
+    return { keyword: "toArrowFunc" };
+  };
+};
+
+let Arrow = () => () => ({ keyword: "toArrowFunc" });
+
+
